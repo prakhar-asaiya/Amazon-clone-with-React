91a9293fae12f0d022bd481a765a0f1af955b713
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2021-06-01 20:09:26 +0530

    Added the 'Add to Basket' functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../../StateProvider";
 
-function Header() {  
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
-      
+      <Link to="/">
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
-      
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -22,7 +26,7 @@ function Header() {
         
           <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+            <span className="header__optionLineTwo">Sign in</span>
           </div>
         
 
@@ -39,14 +43,14 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        
+        <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
-              0
+              {basket?.length}
             </span>
           </div>
-        
+        </Link>
       </div>
     </div>
   );
