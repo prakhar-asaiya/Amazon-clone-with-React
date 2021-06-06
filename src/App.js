6fe624468a36380b1b6cc22d5ce2beb0528daedb
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2021-06-06 11:32:59 +0530

    Updated login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./components/home/Home";
-import Header from "./components/header/Header"
-import Login from "./components/login/Login"
+import Header from "./components/header/Header";
+import Login from "./components/login/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Switch>
+        <Route path="/login">
+            <Login/>
+          </Route>
           <Route path="/">
             <Header />
             <Home />
-          </Route>
-          <Route path="/login">
-            <Login/>
           </Route>
         </Switch>
       </Router>
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,9 +4,34 @@ import { Link, useHistory } from "react-router-dom";
 import { auth } from "../../firebase";
 
 function Login() {
-    
+    const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
 
     return (
         <div className='login'>
@@ -27,7 +52,7 @@ function Login() {
                     <h5>Password</h5>
                     <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button type='submit' className='login__signInButton'>Sign In</button>
+                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
@@ -35,7 +60,7 @@ function Login() {
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
     )
